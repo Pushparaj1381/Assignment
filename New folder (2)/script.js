70eb09e44d86816c36ca7e35f98fe19b003603ab
Pushparaj1381 <115880885+Pushparaj1381@users.noreply.github.com>
2022-11-11 08:27:35 +0530

    Assignment of Java script

---
+++ New folder (2)/script.js
@@ -0,0 +1,25 @@
+let a = prompt("put the value of a")
+let b = prompt("put the value of b")
+let c = prompt("put the value of c")
+let d = prompt("put the value of d")
+let e = prompt("put the value of e")
+
+if(a>b && a>c && a>d && a>e){
+    alert(a+ "is the gretest no.")
+}
+else if
+(b>a && b>c && b>d && b>e){
+    alert(b+ "is the gretest no.")
+}
+else if
+(c>a && c>b && c>d && c>e){
+    alert(c+ "is the gretest no.")
+}
+else if
+(d>a && d>b && d>c && d>e){
+    alert(d+ "is the gretest no.")
+}
+else 
+{
+    alert(e+ "is the gretest no.")
+}
